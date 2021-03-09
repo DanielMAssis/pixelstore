9e476466252a2817e03f34b13f98a79d53d19ae0
DanielMAssis <danielsenai123@gmail.com>
2021-03-09 18:44:27 -0300

    init

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.fa-bars').click(function(){
+        $('#menu').slideToggle();
+    });
+    $('#menu li').click(function(){
+        $('#menu').slideToggle();
+    });
+});
