401e423eb751f0d15b0349f5f18d0b2295f05dc7
DanielMAssis <danielsenai123@gmail.com>
2021-03-09 19:05:37 -0300

    init

--- js/index.js
+++ js/index.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
     $('#menu li').click(function(){
         $('#menu').slideToggle();
     });
+    $('#menu li').on('click', function(){
+        let id = this.id;
+        if(id == 'geral'){
+            $('.card').fadeIn(700);
+            $('.card').css('display','block');
+        }
+        else {
+            $('.card').css('display','none');
+            $('.'+id).fadeIn(700);
+            $('.'+id).css('display','block');
+        }
+    });
 });
